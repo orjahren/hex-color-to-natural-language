10cdd0db753f824520c8406d2ccf2e22d92c1ef5
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-01-25 15:37:05 +0100

    Fix running of tests.

--- tests.ts
+++ tests.ts
@@ -1,21 +1,17 @@
 import hexToColorName from ".";
 
-const runTests = () => {
-  const testcase = (hex: string, expected: string) => {
-    const result = hexToColorName(hex);
-    console.log(`${result === expected ? "✅" : "❌"} ${hex} -> ${result}`);
-  };
-
-  const tests = [
-    ["#ff0000", "red"],
-    ["#72b2f4", "cornflowerblue"],
-    ["#367c2b", "forestgreen"],
-    ["#008000", "green"],
-  ];
-
-  for (const [hex, expected] of tests) {
-    testcase(hex, expected);
-  }
+const testcase = (hex: string, expected: string) => {
+  const result = hexToColorName(hex);
+  console.log(`${result === expected ? "✅" : "❌"} ${hex} -> ${result}`);
 };
 
-runTests();
+const tests = [
+  ["#ff0000", "red"],
+  ["#72b2f4", "cornflowerblue"],
+  ["#367c2b", "forestgreen"],
+  ["#008000", "green"],
+];
+
+for (const [hex, expected] of tests) {
+  testcase(hex, expected);
+}
